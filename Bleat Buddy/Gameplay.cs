117a3e73f900a2194c34d33e6d2403706ce98b08
Denis Zolin <mrdastale712@gmail.com>
2025-10-26 19:47:51 +0300

    Movement update

---
+++ Bleat Buddy/Gameplay.cs
@@ -0,0 +1,66 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace Bleat_Buddy
+{
+    internal class Gameplay : UserControl
+    {
+        Goat goat = new Goat();
+        private Button goatBtn;
+
+        public void FirstScreen()
+        {
+            goatBtn = goat.CreateGoat(new Point(90, 900));
+            Controls.Add(goatBtn);
+        }
+        // Меню паузы
+        private void PauseMenu()
+        {
+            Label pauseLbl = new Label();
+            Button continueBtn = new Button();
+            Button settingsBtn = new Button();
+            Button exitBtn = new Button();
+            BackColor = Color.Black;
+
+            // Пауза
+            pauseLbl.Location = new Point(890, 390);
+            pauseLbl.Size = new Size(148, 60);
+            pauseLbl.Font = new Font("Segoe Script", 28);
+            pauseLbl.Text = "Пауза";
+            Controls.Add(pauseLbl);
+            // Продолжить
+            continueBtn.Location = new Point(540, 500);
+            continueBtn.Size = new Size(250, 80);
+            continueBtn.Text = "Продолжить";
+            continueBtn.Visible = true;
+            Controls.Add(continueBtn);
+            // Настройки
+            settingsBtn.Location = new Point(835, 500);
+            settingsBtn.Size = new Size(250, 80);
+            settingsBtn.Text = "Настройки";
+            settingsBtn.Visible = true;
+            Controls.Add(settingsBtn);
+            // Выход
+            exitBtn.Location = new Point(1130, 500);
+            exitBtn.Size = new Size(250, 80);
+            exitBtn.Text = "Выйти";
+            exitBtn.Visible = true;
+            Controls.Add(exitBtn);
+        }
+
+        public void KeyPress(KeyEventArgs e)
+        {
+            goat.Goat_Movemant(goatBtn, e);
+        }
+        public void EscapeButton(KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Escape:
+                    Controls.Clear();
+                    PauseMenu();
+                    break;
+            }
+        }
+    }
+}
---
+++ Bleat Buddy/Goat.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Bleat_Buddy 
+{
+    internal class Goat : UserControl
+    {
+        private static string gameDirectory = Directory.GetCurrentDirectory();
+        // Текстуры козла
+        static string projectRoot = Path.GetFullPath(Path.Combine(Environment.CurrentDirectory, @"..\..\"));
+        private Image goatRight_Texture = Image.FromFile(Path.Combine(projectRoot, "resurse", "textureRight.png"));
+        private Image goatLeft_Texture = Image.FromFile(Path.Combine(projectRoot, "resurse", "textureLeft.png"));
+        Button goat = new Button();
+        private int level;
+        int speed;
+        // Словарь возраст - текстура козла
+        private Dictionary<string, string> goatTypes = new Dictionary<string, string>
+        {
+            { "little", "littleGoat.png"},
+            { "teenager", "teenagerGoat.png"},
+            { "adult", "adultGoat.png"},
+            { "elderly", "elderlyGoat.png"}
+        };
+
+        // Конструктор класса
+        public Goat()
+        {
+            this.speed = 10;
+        }
+
+        // Создание кнопки-козла
+        public Button CreateGoat(Point location)
+        {
+            goat= new Button();
+            goat.Size = new Size(90, 90);
+            goat.Location = location;
+            goat.BackColor = this.BackColor;
+            goat.BackgroundImage = goatRight_Texture;
+            goat.BackgroundImageLayout = ImageLayout.Stretch;
+            goat.FlatStyle = FlatStyle.Flat;
+            goat.FlatAppearance.BorderSize = 0;
+            return goat;
+        }
+
+        // Обработка движения козла
+        public void Goat_Movemant(Button goatBtn, KeyEventArgs e)
+        {
+            int leftBound = goatBtn.Left;
+
+            switch (e.KeyCode)
+            {
+                case Keys.A:
+                    if (leftBound > 0)
+                    {
+                        goatBtn.Left -= speed;
+                        goat.BackgroundImage = goatLeft_Texture;
+                        goat.BackgroundImageLayout = ImageLayout.Stretch;
+                        goat.FlatStyle = FlatStyle.Flat;
+                        goat.FlatAppearance.BorderSize = 0;
+                        break;
+                    }
+                    break;
+                case Keys.D:
+                    if (leftBound + 90 < Screen.PrimaryScreen.Bounds.Width)
+                    {
+                        goatBtn.Left += speed;
+                        goat.BackgroundImage = goatRight_Texture;
+                        goat.BackgroundImageLayout = ImageLayout.Stretch;
+                        goat.FlatStyle = FlatStyle.Flat;
+                        goat.FlatAppearance.BorderSize = 0;
+                        break;
+                    }
+                    break;
+            }
+        }
+    }
+}
